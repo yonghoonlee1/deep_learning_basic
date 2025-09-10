5e2cd4e21ca1ee41b66478cb9d46c8802731571c
yonghoonlee <yonghoonlee111@gmail.com>
2025-09-10 14:58:10 +0900

    initial commit

---
+++ rnn.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+class MyRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = torch.tanh(self.i2h(combined))
+        output = self.h2o(hidden)
+        return output, hidden
+
+    def get_hidden(self, batch_size=1, device="cpu"):
+        return torch.zeros(batch_size, self.hidden_size, device=device)
